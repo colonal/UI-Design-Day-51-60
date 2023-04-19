3eaa1174ac2c70f11c4b14c40867ba3277789200
Mohammad Ebbeni <colonal.ch@gmail.com>
2023-04-19 16:28:43 +0300

    Home Screen

---
+++ lib/home/screens.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:ui_design_day_51_60/day52/page/starte_screen.dart';
+import 'package:ui_design_day_51_60/day53/screen/start_screen.dart';
+import 'package:ui_design_day_51_60/day54/screen/home_screen.dart';
+import 'package:ui_design_day_51_60/day55/screen/start_screen.dart';
+import 'package:ui_design_day_51_60/day56/screen/home_screen.dart';
+import 'package:ui_design_day_51_60/day57/screen/home_screen.dart';
+import 'package:ui_design_day_51_60/day58/screen/home_screen.dart';
+import 'package:ui_design_day_51_60/day59/screen/start_screen.dart';
+import 'package:ui_design_day_51_60/day60/screen/start_screen.dart';
+
+import '../day51/screen/starte_screen.dart';
+
+
+const List screens = [
+  [
+    "Day 51",
+    "NFT Marketplace",
+    "screenshot/day51/1.png",
+    Day51StareScreen(),
+    Color(0xffEEEEEE),
+  ],
+  [
+    "Day 52",
+    "Fitness Workout",
+    "screenshot/day52/1.png",
+    StartScreenDay52(),
+    Color(0xffD79F01),
+  ],
+  [
+    "Day 53",
+    "Agency App Design",
+    "screenshot/day53/1.png",
+    StartScreenDay53(),
+    Color(0xff3A4E6B),
+  ],
+  [
+    "Day 54",
+    "Coffee Delivery App",
+    "screenshot/day54/1.png",
+    StartScreenDay54(),
+    Color(0xffFEFFFB),
+  ],
+  [
+    "Day 55",
+    "Food ordering",
+    "screenshot/day55/1.png",
+    StartScreenDay55(),
+    Color(0xff9C658C),
+  ],
+  [
+    "Day 56",
+    "NFT Wallet App",
+    "screenshot/day56/1.png",
+    StartScreenDay56(),
+    Color(0xff5D5C7F),
+  ],
+  [
+    "Day 57",
+    "Messenger App",
+    "screenshot/day57/1.png",
+    StartScreenDay57(),
+    Color(0xff9646FE),
+  ],
+  [
+    "Day 58",
+    "Nike Shoes",
+    "screenshot/day58/1.png",
+    StartScreenDay58(),
+    Color(0xffE8F4F2),
+  ],
+  [
+    "Day 59",
+    "Mindfulness and Meditation",
+    "screenshot/day59/1.png",
+    StartScreenDay59(),
+    Color(0xff3960DC),
+  ],
+  [
+    "Day 60",
+    "Pokemon Gaming",
+    "screenshot/day60/1.png",
+    StartScreenDay60(),
+    Color(0xff1E1E1E),
+  ],
+];
